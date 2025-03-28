b3e0d4675c2db5d77ab70bc8f357bb9f93badcf6
Ulorewien <rohanshingre7@gmail.com>
2025-03-27 18:20:00 -0700

    Updated data cleaning pipeline

--- model_distillation.py
+++ model_distillation.py
@@ -9,17 +9,11 @@ data = load_dataset("mteb/tweet_sentiment_extraction")
 
 start_idx, end_idx = 0, 5000
 output_folder = "data\\train"
-to_csv(data, start_idx, end_idx, output_folder)
-raw_data = pd.read_csv(os.path.join(output_folder, f"twitter_sentiment_{start_idx}_{end_idx}.csv"))
-final_data = raw_data[raw_data["text"].notna() & (raw_data["text"].str.strip() != "")]
-final_data.to_csv(os.path.join(output_folder, f"twitter_sentiment_{start_idx}_{end_idx}_cleaned.csv"), index=False)
+clean_data(data, start_idx, end_idx, output_folder)
 
 start_idx, end_idx = 5001, 6000
 output_folder = "data\\test"
-to_csv(data, start_idx, end_idx, output_folder)
-raw_data = pd.read_csv(os.path.join(output_folder, f"twitter_sentiment_{start_idx}_{end_idx}.csv"))
-final_data = raw_data[raw_data["text"].notna() & (raw_data["text"].str.strip() != "")]
-final_data.to_csv(os.path.join(output_folder, f"twitter_sentiment_{start_idx}_{end_idx}_cleaned.csv"), index=False)
+clean_data(data, start_idx, end_idx, output_folder)
 
 
 chain_of_thought_prompt = get_prompt("prompts/llama_3_1_prompt.json")
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import json
+import pandas as pd
 
 def to_csv(data, start, end, output_folder):
     output_path = os.path.join(output_folder, f"twitter_sentiment_{start}_{end}.csv")
@@ -17,6 +18,12 @@ def to_csv(data, start, end, output_folder):
             writer.writerow(row)
             
     print(f"Saved {output_path}")
+    
+def clean_data(data, start_idx, end_idx, output_folder):
+    to_csv(data, start_idx, end_idx, output_folder)
+    raw_data = pd.read_csv(os.path.join(output_folder, f"twitter_sentiment_{start_idx}_{end_idx}.csv"))
+    final_data = raw_data[raw_data["text"].notna() & (raw_data["text"].str.strip() != "")]
+    final_data.to_csv(os.path.join(output_folder, f"twitter_sentiment_{start_idx}_{end_idx}_cleaned.csv"), index=False)
     
 def get_prompt(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
